296e7189e1583a81f1bec71e7310e068afd42920
mtehver <mark.tehver@gmail.com>
2021-09-01 12:41:17 +0300

    Ordering

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
             targets: ["CartoMobileSDK"]
         )
     ],
+    dependencies: [
+    ],
     targets: [
         .binaryTarget(
             name: "CartoMobileSDK",
             url: "https://nutifront.s3.amazonaws.com/sdk_snapshots/sdk4-ios-4.4.1.zip",
             checksum: "d7365c13a52682472cfe2ffb4cd77b1fd1038dbca60f32daa67ea721c35535af"
         )
-    ],
-    dependencies: [
     ]
 )
