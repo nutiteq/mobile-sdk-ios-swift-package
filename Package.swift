9feebfb4a5210c394442b4c61b022f0296a834f9
mtehver <mark.tehver@gmail.com>
2021-09-01 12:25:42 +0300

    Version 4.4.1

---
+++ Package.swift
@@ -0,0 +1,23 @@
+import PackageDescription
+
+let package = Package(
+    name: "CartoMobileSDK",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "CartoMobileSDK",
+            targets: ["CartoMobileSDK"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "CartoMobileSDK",
+            url: "https://nutifront.s3.amazonaws.com/sdk_snapshots/sdk4-ios-4.4.1.zip",
+            checksum: "d7365c13a52682472cfe2ffb4cd77b1fd1038dbca60f32daa67ea721c35535af"
+        )
+    ],
+    dependencies: [
+    ]
+)
